a44567f6ff7d29aa98c7ea299921a8cfb61a9145
Sadat <shadowofdevil151@gmail.com>
2026-04-30 21:46:52 +0600

    send this to git

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import dotenv from "dotenv/config"
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 
 
-connectDB()
+connectDB().then(()=>{
+    const port=process.env.PORT || 3000
+    app.on("error",(error)=>{
+        console.log(`App crashed!! Error: ${error}`);
+    })
+    app.listen(port,()=>{
+        console.log(`Server listening on port: ${port}`);
+    })    
+}).catch((error)=>{
+    console.log(`Database connection failed!! Error: ${error}`);
+    process.exit(1)
+})
